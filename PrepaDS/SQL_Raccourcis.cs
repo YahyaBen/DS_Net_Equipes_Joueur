cf8918f085e9885aec8bbac99e7352da39a55f66
BENABDALLAH Yahya <yahya.ben.abdallah@gmail.com>
2020-01-06 14:29:27 +0100

    1- Ajout de la Base de donnees

---
+++ PrepaDS/SQL_Raccourcis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrepaDS
+{
+    class SQL_Raccourcis
+    {
+        // Declaration
+        public SqlConnection Connex = new SqlConnection();
+        public SqlCommand Comm = new SqlCommand();
+        public SqlDataReader Rd;
+
+        public DataTable DT = new DataTable();
+
+        // Declaration methode connecter 
+        public void CONNECTER()
+        {
+            if (Connex.State == ConnectionState.Closed || Connex.State == ConnectionState.Broken)
+            {
+                Connex.ConnectionString = "Data Source=HYRKUL;Initial Catalog=Prepa_DS;Integrated Security=True";
+                Connex.Open();
+            }
+        }
+        // Declaration methode Deconnecter
+        public void DECONNECTER()
+        {
+            if (Connex.State == ConnectionState.Open)
+            {
+                Connex.Close();
+            }
+        }
+    }
+}
